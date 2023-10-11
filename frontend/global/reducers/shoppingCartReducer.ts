aff35234a73177fe6c96a1e390ede04f98dfc62c
aguusscuderi <aguusscuderi9@gmail.com>
2023-10-11 14:54:27 -0300

    solving errors

---
+++ frontend/global/reducers/shoppingCartReducer.ts
@@ -0,0 +1,102 @@
+import { Action } from "../../global/actions/shoppingCartActions";
+import { ActionTypes } from "../../global/types";
+
+type CartItem = {
+    id: string;
+    title: string;
+    imageSource: string;
+    text: string;
+    price: number;
+    quantityToBuy: number
+}
+
+//NO SE SI ESTO ESTA BIEN
+// const initialState = {
+//     cart: [{
+//     id: null,
+//     title: null,
+//     imageSource: null,
+//     text: null,
+//     price: null,
+//     quantityToBuy: 0
+//     }],
+//     cartCount: 0
+// }
+
+const initialState = {
+    cart: [{
+    id: "",
+    title: "",
+    imageSource: "",
+    text: "",
+    price: 0,
+    quantityToBuy: 0
+    }],
+    cartCount: 0
+}
+
+
+
+/*type initialState = {
+    cart: CartItem[]
+    cartCount: number
+}*/
+
+
+export function shoppingCartReducer (state = initialState, action: Action) {
+    switch (action.type){
+        case ActionTypes.ADDTOCART: {
+            const isInCart = (id: string) => {
+                const verificator = state.cart.some(el => el.id === id)
+                return verificator
+            }
+            return ((isInCart(action.payload.id) && action.payload.id != null)  ? {
+                ...state,
+                cart: state.cart.map((product) => 
+                product.id === action.payload.id ? { ...product, quantityToBuy: product.quantityToBuy + action.payload.count} : {...product}
+                ),
+                cartCount: state.cartCount = action.payload.count + state.cartCount,
+            } : {
+                cart: [...state.cart, {...action.payload.item, quantityToBuy: action.payload.count}],
+                cartCount: action.payload.count + state.cartCount,
+            })
+        }
+
+        case ActionTypes.REMOVEFROMCART: {
+            const cartWithoutDeletedElement = state.cart.filter(el => el.id !== action.payload.id)
+            const cartCountWithoutElement = state.cart.find(el => el.id ===  action.payload.id)
+            return {
+                cart: [...cartWithoutDeletedElement],
+                cartCount:( cartCountWithoutElement ? state.cartCount - cartCountWithoutElement.quantityToBuy : state.cartCount)
+            }
+        }
+        
+        case ActionTypes.GETPARSEDCOOKIE: {
+                const isNotEmpty = Object.keys(action.payload.cookie).length != 0
+                if(isNotEmpty) {
+                    const parsedCookie = JSON.parse(action.payload.cookie.shoppingCart)
+                    return {
+                        cart: [...parsedCookie.cart],
+                        cartCount: parsedCookie.counter
+                    }
+                }else{
+                    return {
+                        ...state
+                    }
+                }
+        }
+     
+            
+    
+        case ActionTypes.CLEARCART:{
+            return {
+                cart: {...initialState.cart},
+                cartCount: 0
+            }
+        }
+           
+        
+        default: return state
+
+    }
+}
---
+++ frontend/utils/fetchData.ts
@@ -0,0 +1,83 @@
+const baseUrl = process.env.BASE_URL;
+
+export const getAuth = async (url, header) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        //'x-access-token': header.accessToken,
+        'x-refresh-token': header.refreshToken
+    }, 
+  })
+  const data = await res.json()
+  return data
+}
+
+export const getData = async (url, token?) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+    method: 'GET',
+    headers: {
+        'Authorization': token
+    }, 
+  })
+  const data = await res.json()
+  return data
+}
+
+export const postData = async (url, post, token?) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+      },
+      body: JSON.stringify(post)
+  })
+
+  const data = await res.json()
+  return data
+}
+
+
+
+export const putData = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+      },
+      body: JSON.stringify(post)
+  })
+
+  const data = await res.json()
+  return data
+}
+
+export const patchData = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+      method: 'PATCH',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+      },
+      body: JSON.stringify(post)
+  })
+
+  const data = await res.json()
+  return data
+}
+
+
+export const deleteData = async (url, token) => {
+  const res = await fetch(`${baseUrl}/api/${url}`, {
+      method: 'DELETE',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+      }
+  })
+
+  const data = await res.json()
+  return data
+}
